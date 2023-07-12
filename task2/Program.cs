6a536b200219884a8179d85f020e95f87661eaba
darina1009 <darina_1009@mail.ru>
2023-07-12 15:37:35 +0300

    task2 task3

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int Promt (string text){
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int Summ(int n, int m){
+    if (n==m) return n;
+    else return Summ(n+1, m) + n;
+}
+
+
+int n = Promt("Введите n:");
+int m = Promt("Введите m:");
+
+Console.WriteLine(Summ(n,m));
---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿int Promt (string text){
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Akkerman(int n, int m){
+    if (m == 0) 
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else 
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = Promt("Введите m:");
+int n = Promt("Введите n:");
+
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
