63e83fcef7f5735bba5abb6ab1564da243d2e365
darina1009 <darina_1009@mail.ru>
2023-07-12 15:35:50 +0300

    lesson9

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿int Promt (string text){
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int Summ(int n, int m){
+    if (n==m) return n;
+    else 
+    Console.Write($"{Summ(n, m+1)}, ");
+    return m;
+}
+
+
+int n = Promt("Введите n:");
+
+if (n<1){
+    Console.WriteLine("Введите число:");
+    return;
+}
+Console.WriteLine(Summ(n,1));
